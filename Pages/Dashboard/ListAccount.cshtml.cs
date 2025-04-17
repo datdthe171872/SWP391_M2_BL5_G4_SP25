4127eed579de8931ea8a5d95c40bb685c88578ff
phuoclh <155793629+Phuoclee03@users.noreply.github.com>
2025-04-17 20:54:15 +0700

    Revert "update view of list account"
    This reverts commit 374211fa71fb4828ac03bb9b252b4dd211f08d71.

--- Pages/Dashboard/ListAccount.cshtml.cs
+++ Pages/Dashboard/ListAccount.cshtml.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using SWP391_M2_BL5_G4_SP25.DTO.UserDtos;
 using SWP391_M2_BL5_G4_SP25.Models;
 using System.Threading.Tasks;
 
@@ -11,31 +10,16 @@ namespace SWP391_M2_BL5_G4_SP25.Pages.Dashboard
 	public class ListAccountModel : PageModel
 	{
 		private readonly UserManager<User> _userManager;
-		private readonly RoleManager<Role> _roleManager;
 
-		public ListAccountModel(UserManager<User> userManager, RoleManager<Role> roleManager)
+		public ListAccountModel(UserManager<User> userManager)
 		{
 			_userManager = userManager;
-			_roleManager = roleManager;
 		}
-
-		public List<UserDto> Users { get; set; }
+		public IEnumerable<User> Users { get; set; }
 
 		public async Task OnGetAsync()
 		{
-			var users = await _userManager.Users.ToListAsync();
-
-			Users = new List<UserDto>();
-
-			foreach (var user in users)
-			{
-				var roles = await _userManager.GetRolesAsync(user);
-				Users.Add(new UserDto
-				{
-					User = user,
-					Roles = roles.ToList()
-				});
-			}
+			Users = await _userManager.Users.Where(u => !u.isDelete).ToListAsync();
 		}
 	}
 }
