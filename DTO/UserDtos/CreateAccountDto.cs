9c91e7135add430291adaf24067fc0314d3bfff9
phuoclh <155793629+Phuoclee03@users.noreply.github.com>
2025-04-17 20:52:02 +0700

    Revert "add paging"
    This reverts commit b1661cf1cc42b9ee32ee9a53d178b0127492f19d.

--- DTO/UserDtos/CreateAccountDto.cs
+++ DTO/UserDtos/CreateAccountDto.cs
@@ -14,6 +14,17 @@ namespace SWP391_M2_BL5_G4_SP25.DTO.UserDtos
 		[Display(Name = "Email")]
 		public string Email { get; set; }
 
+		//[Required]
+		//[StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+		//[DataType(DataType.Password)]
+		//[Display(Name = "Password")]
+		//public string Password { get; set; }
+
+		//[DataType(DataType.Password)]
+		//[Display(Name = "Confirm password")]
+		//[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+		//public string ConfirmPassword { get; set; }
+
 		[StringLength(255)]
 		[Display(Name = "Address")]
 		public string? Address { get; set; }
