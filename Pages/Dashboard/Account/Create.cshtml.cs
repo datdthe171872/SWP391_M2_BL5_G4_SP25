99c33cfeac0d8f0e27358bf11888417079efe7c6
HoangQuan <155793629+Phuoclee03@users.noreply.github.com>
2025-04-17 17:39:36 +0700

    change folder and add create html

---
+++ Pages/Dashboard/Account/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SWP391_M2_BL5_G4_SP25.Pages.Dashboard.Account
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
