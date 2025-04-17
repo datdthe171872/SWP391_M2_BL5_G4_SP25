a4c60c937df5c94ae511cb961e370a9cfafe54c2
phuoclh <155793629+Phuoclee03@users.noreply.github.com>
2025-04-17 20:54:18 +0700

    Revert "add layout and list account in dashboard"
    This reverts commit 4adfbb75875bcf6533c426af16192e72136f2371.

--- Models/User.cs
+++ Models/User.cs
@@ -6,20 +6,22 @@ using System.Data;
 
 namespace SWP391_M2_BL5_G4_SP25.Models
 {
-	public class User : IdentityUser<int>
-	{
-		[Required]
-		[StringLength(100)]
-		public string FullName { get; set; }
+    public class User : IdentityUser<int>
+    {
 
-		[StringLength(255)]
-		public string? Address { get; set; }
-		public DateTime CreatedAt { get; set; } = DateTime.Now;
-		public bool isDelete { get; set; }//0.inactive, 1. active
-		public ClientProfile ClientProfile { get; set; }
-		public JobSeekerProfile JobSeekerProfile { get; set; }
-		public List<JobApplication> JobApplications { get; set; }
-		public List<CompanyReview> CompanyReviews { get; set; }
-		public List<Notification> Notifications { get; set; }
-	}
+       
+        [Required]
+        [StringLength(100)]
+        public string FullName { get; set; }
+
+        [StringLength(255)]
+        public string? Address { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public bool isDelete { get; set; }
+        public ClientProfile ClientProfile { get; set; }
+        public JobSeekerProfile JobSeekerProfile { get; set; }
+        public List<JobApplication> JobApplications { get; set; }
+        public List<CompanyReview> CompanyReviews { get; set; }
+        public List<Notification> Notifications { get; set; }
+    }
 }
