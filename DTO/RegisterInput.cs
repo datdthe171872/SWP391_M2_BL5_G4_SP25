5636b4da723fb7b8010c28a1ae8f3655b896b91c
datdthe171872 <datdthe171872@fpt.edu.vn>
2025-04-22 21:20:40 +0700

    fix regisster input

--- DTO/RegisterInput.cs
+++ DTO/RegisterInput.cs
@@ -1,9 +1,14 @@
-﻿namespace SWP391_M2_BL5_G4_SP25.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SWP391_M2_BL5_G4_SP25.DTO
 {
     public class RegisterInput
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
         public bool RoleType { get; set; }
+        [Required]
         public string Fullname { get; set; }
         public string Password { get; set; }
     }
