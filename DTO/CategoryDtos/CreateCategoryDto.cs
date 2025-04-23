5693b84a4c6d416e6ddcdae59fad0e670b750a0c
phuoclh <phuoclhhe172380@fpt.edu.vn>
2025-04-23 21:03:52 +0700

    add create in ManageCategory

---
+++ DTO/CategoryDtos/CreateCategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SWP391_M2_BL5_G4_SP25.DTO.CategoryDtos
+{
+	public class CreateCategoryDto
+	{
+		[Required]
+		[StringLength(50)]
+		[Display(Name = "Name")]
+		public string CategoryName { get; set; }
+		[Required]
+		public string Description { get; set; }
+	}
+}
