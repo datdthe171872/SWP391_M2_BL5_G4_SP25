3f5460af8f192aab9e16a1774b6156e1b0859a2c
phuoclh <phuoclhhe172380@fpt.edu.vn>
2025-04-23 20:15:01 +0700

    add search in ManageCategory

---
+++ DTO/CategoryDtos/CategorySearchDto.cs
@@ -0,0 +1,7 @@
+﻿namespace SWP391_M2_BL5_G4_SP25.DTO.CategoryDtos
+{
+	public class CategorySearchDto
+	{
+		public string? SearchString { get; set; }
+	}
+}
--- Pages/Admin/Category/Index.cshtml.cs
+++ Pages/Admin/Category/Index.cshtml.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using SWP391_M2_BL5_G4_SP25.DTO.CategoryDtos;
+using SWP391_M2_BL5_G4_SP25.DTO.UserDtos;
 using SWP391_M2_BL5_G4_SP25.Models;
 using System.Linq;
 
@@ -17,9 +19,20 @@ namespace SWP391_M2_BL5_G4_SP25.Pages.Admin.Category
 
 		public List<JobCategory> Categories { get; set; } = new List<JobCategory>();
 
+		[BindProperty(SupportsGet = true)]
+		public CategorySearchDto SearchInput { get; set; } = new CategorySearchDto();
+
 		public async Task OnGetAsync()
 		{
-			Categories = await _context.JobCategories.Where(c=>c.isDelete==false).ToListAsync();
+			var query = _context.JobCategories.Where(c => !c.isDelete);
+
+			if (!string.IsNullOrWhiteSpace(SearchInput.SearchString))
+			{
+				query = query.Where(c => c.CategoryName.ToLower().Contains(SearchInput.SearchString.ToLower()) || 
+				c.Description.ToLower().Contains(SearchInput.SearchString.ToLower()));
+			}
+
+			Categories = await query.ToListAsync();
 		}
 
 		public async Task<IActionResult> OnPostDeleteAsync(int id)
