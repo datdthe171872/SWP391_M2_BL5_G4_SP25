a9b5f0d37ddaa7ba489dba8025c7bcd65312a58b
datdthe171872 <datdthe171872@fpt.edu.vn>
2025-04-23 20:13:58 +0700

    fix register

--- Pages/Account/Register.cshtml.cs
+++ Pages/Account/Register.cshtml.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 using SWP391_M2_BL5_G4_SP25.DTO;
 using SWP391_M2_BL5_G4_SP25.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SWP391_M2_BL5_G4_SP25.Pages.Account
 {
@@ -24,7 +26,12 @@ namespace SWP391_M2_BL5_G4_SP25.Pages.Account
         }
         public async Task<IActionResult> OnPostAsync()
         {
-
+            var userExist = await _userManager.FindByEmailAsync(Input.Email);
+            if(userExist != null)
+            {
+                ModelState.AddModelError(string.Empty,"Email existed!");
+                return Page();
+            }
             var user = new User {Email = Input.Email, FullName = Input.Fullname,UserName = Input.Email };
             var result = await _userManager.CreateAsync(user, Input.Password);
 
