570b34f6d3aaa6c010be65c1cfaa1d864e138403
datdthe171872 <datdthe171872@fpt.edu.vn>
2025-04-25 02:10:34 +0700

    save

---
+++ Pages/Client/AppliedJob.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SWP391_M2_BL5_G4_SP25.Pages.Client
+{
+    public class AppliedJobModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
