e870f9cd971963f99f827c5d83b2006bcaeef254
datdthe171872 <datdthe171872@fpt.edu.vn>
2025-04-27 09:49:50 +0700

    done

---
+++ DTO/AppliedJobSeeker.cs
@@ -0,0 +1,16 @@
+﻿using SWP391_M2_BL5_G4_SP25.Models;
+
+namespace SWP391_M2_BL5_G4_SP25.DTO
+{
+    public class AppliedJobSeeker
+    {
+        public int JobApplicationId { get; set; }
+        public string Logo { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public DateTime AppliedDate { get; set; }
+        public string CV {  get; set; }
+        public string Status { get; set; }
+        public bool FileExist { get; set; }
+    }
+}
---
+++ DTO/ManageAppliedSeekerInput.cs
@@ -0,0 +1,9 @@
+﻿namespace SWP391_M2_BL5_G4_SP25.DTO
+{
+    public class ManageAppliedSeekerInput
+    {
+        public int JobApplicationId {  get; set; }
+        public string Type { get; set; }
+        public string Link { get; set; }    
+    }
+}
