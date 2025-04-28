c4fff54938cf5880b4857b6f82732787c7817bf7
phuoclh <phuoclhhe172380@fpt.edu.vn>
2025-04-28 21:45:01 +0700

    update create company

---
+++ DTO/CreateCompanyDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SWP391_M2_BL5_G4_SP25.DTO
+{
+	public class CreateCompanyDto
+	{
+		[Required]
+		[StringLength(100)]
+		public string CompanyName { get; set; }
+
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+
+		public string? Description { get; set; }
+
+		public string? Link { get; set; }
+
+		[StringLength(255)]
+		public string? Location { get; set; }
+
+		public IFormFile? ImageFile { get; set; }
+	}
+}
---
+++ Pages/Company/Create.cshtml.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SWP391_M2_BL5_G4_SP25.DTO;
+using SWP391_M2_BL5_G4_SP25.Models;
+using SWP391_M2_BL5_G4_SP25.Service;
+
+namespace SWP391_M2_BL5_G4_SP25.Pages.Company
+{
+	public class CreateModel : PageModel
+	{
+		private readonly MyDBContext _context;
+		private readonly UploadImg _uploadImg;
+		private readonly UserManager<User> _userManager;
+
+		public CreateModel(MyDBContext context, UploadImg uploadImg, UserManager<User> userManager)
+		{
+			_context = context;
+			_uploadImg = uploadImg;
+			_userManager = userManager;
+		}
+
+		[BindProperty]
+		public CreateCompanyDto Input { get; set; } = new CreateCompanyDto();
+
+		public async Task<IActionResult> OnGet()
+		{
+			var user = await _userManager.GetUserAsync(User);
+			if (user == null)
+			{
+				return RedirectToPage("/Account/Login");
+			}
+
+			var clientProfile = await _context.ClientProfiles
+					.FirstOrDefaultAsync(cp => cp.UserID == user.Id && !cp.isDelete);
+
+			if (clientProfile == null)
+			{
+				TempData["StatusMessage"] = "You need to create a client profile first";
+				return RedirectToPage("/ClientProfiles/Create");
+			}
+
+			return Page();
+		}
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
+
+			var user = await _userManager.GetUserAsync(User);
+			if (user == null)
+			{
+				return RedirectToPage("/Account/Login");
+			}
+
+			var clientProfile = await _context.ClientProfiles
+					.FirstOrDefaultAsync(cp => cp.UserID == user.Id && !cp.isDelete);
+
+			if (clientProfile == null)
+			{
+				TempData["StatusMessage"] = "You need to create a client profile first";
+				return RedirectToPage("/ClientProfiles/Create");
+			}
+
+			string imagePath = null;
+			if (Input.ImageFile != null)
+			{
+				imagePath = await _uploadImg.UploadAsync(Input.ImageFile, "uploads/companies");
+			}
+
+			var company = new Models.Company
+			{
+				CompanyName = Input.CompanyName,
+				Email = Input.Email,
+				Description = Input.Description,
+				Link = Input.Link,
+				Location = Input.Location,
+				Image = imagePath,
+				ClientProfileID = clientProfile.ClientProfileID, 
+				isDelete = false
+			};
+
+			_context.Companies.Add(company);
+			await _context.SaveChangesAsync();
+			return RedirectToPage("CompanyList");
+		}
+	}
+}
