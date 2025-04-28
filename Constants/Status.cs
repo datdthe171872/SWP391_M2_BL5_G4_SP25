136b4a6578b0c6a0c6f469261b32b56114bf5815
datdthe171872 <datdthe171872@fpt.edu.vn>
2025-04-28 22:17:21 +0700

    add status

---
+++ Constants/Status.cs
@@ -0,0 +1,22 @@
+﻿namespace SWP391_M2_BL5_G4_SP25.Constants
+{
+    public static class StatusJob
+    {
+        public const string OPEN = "Open";
+        public const string WAIT = "Wait";
+        public const string END = "End";
+    }
+
+    public static class StatusJobApply
+    {
+        public const string COMFIRM = "Comfirm";
+        public const string REJECT = "Reject";
+        public const string PENDING = "Pending";
+    }
+
+    public static class JoptypeOption
+    {
+        public const string FULL_TIME = "Full-time";
+        public const string PART_TIME = "Part-time";
+    }
+}
